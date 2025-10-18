e501051a478a0dd64b52a9dccd79d30e298f70b6
Jakub Filipek <jakub20021434@gmail.com>
2025-10-18 15:22:43 +0200

    Actuator security demo

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/jf/spring/JFApp.java
@@ -0,0 +1,12 @@
+package com.jf.spring;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JFApp {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(JFApp.class, args);
+	}
+
+}
