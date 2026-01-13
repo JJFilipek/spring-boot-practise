48735617b8c059389f6f8a536dea85fd9a00554b
Jakub Filipek <jakub20021434@gmail.com>
2026-01-13 08:26:12 +0100

    Spring Boot REST POJO

---
+++ 04-spring-boot-rest-crud/01-sprinng-boot-rest-crud/src/main/java/com/jf/demo/rest/StudentRestController.java
@@ -0,0 +1,26 @@
+package com.jf.demo.rest;
+
+import com.jf.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students" - return list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Andrzej", "Nowacki"));
+        theStudents.add(new Student("Jan", "Rowacki"));
+        theStudents.add(new Student("Adam", "Nora"));
+        return theStudents;
+    }
+
+}
